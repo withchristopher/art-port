d034bbc908416517dad3c3bf7da6a011cacbbcff
Chris Maxwell <chmaxwel@icloud.com>
2020-10-05 17:18:22 +0200

    ++about

---
+++ src/components/about/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+    return(<div>
+         <section>
+            <h1 id="about">Who am I?</h1>
+            <p>Art Port is an art shop application, for you to upload your artwork and share with the world.</p>
+        </section>
+    </div>
+    )
+};
+
+export default About;
