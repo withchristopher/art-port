5bad4aff87a703617c3795bfc038a6c6c46defe2
Chris Maxwell <chmaxwel@icloud.com>
2020-10-06 20:51:25 +0200

    Create index.js

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Gallery() {
+    return (
+        <section>
+            <h1>NAME</h1>
+            <p>DESCRIPTION</p>
+        </section>
+    );
+}
+
+export default Gallery;
