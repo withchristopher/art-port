44a5a50f2393f3bab6bc593945fef35be0421d4b
Chris Maxwell <chmaxwel@icloud.com>
2020-10-07 22:34:42 +0200

    Updated PhotoList

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -3,14 +3,14 @@ import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Gallery({ currentCategory }) {
-    const { name, description } = currentCategory;
-    return (
-        <section>
-            <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
-            <p>{description}</p>
-            <PhotoList />
-        </section>
-    );
+  const { name, description } = currentCategory;
+  return (
+    <section>
+      <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
+      <p>{description}</p>
+      <PhotoList  />
+    </section>
+  );
 }
 
 export default Gallery;
