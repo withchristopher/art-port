855e14bab953e34eb628381f8c0b3508f7643d81
Chris Maxwell <chmaxwel@icloud.com>
2020-10-07 16:46:51 +0200

    lifting state to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import About from './components/About/index';
 import Nav from './components/Nav/index';
 import Gallery from './components/Gallery/index';
 
 function App() {
+    const [categories] = useState([
+        {
+          name: 'commercial',
+          description: 'Photos of grocery stores, food trucks, and other commercial projects',
+        },
+        { name: 'portraits', description: 'Portraits of people in my life' },
+        { name: 'food', description: 'Delicious delicacies' },
+        { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+      ]);
+      
+    const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
 
     return (
         <div>
-            <Nav></Nav>
+            <Nav
+                categories={categories}
+                setCurrentCategory={setCurrentCategory}
+                currentCategory={currentCategory}
+            ></Nav>
             <main>
                 <Gallery></Gallery>
                 <About></About>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
-function Nav() {
-const [currentCategory, setCurrentCategory] = useState(categories[0]);
-const [categories] = useState([
-  {
-    name: 'commercial',
-    description: 'Photos of grocery stores, food trucks, and other commercial projects',
-  },
-  { name: 'portraits', description: 'Portraits of people in my life' },
-  { name: 'food', description: 'Delicious delicacies' },
-  { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-]);
+function Nav(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory,
+    } = props;
+
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
+    
 
   return (
     <header className="flex-row px-1">
