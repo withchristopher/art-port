a7a3686f6a8bfdec187b6c2f2ac06863f8f19909
Chris Maxwell <chmaxwel@icloud.com>
2020-10-08 11:59:07 +0200

    updated props

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -4,11 +4,12 @@ import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Gallery({ currentCategory }) {
   const { name, description } = currentCategory;
+
   return (
     <section>
       <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
       <p>{description}</p>
-      <PhotoList  />
+      <PhotoList category={currentCategory.name} />
     </section>
   );
 }
